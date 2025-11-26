5ffd7af3379c932d2fa5f3daa07d828151436d17
Prapavarine <prapavarine.c@buzzebess.com>
2025-11-26 20:36:58 +0700

    update contact form

--- src/components/ui/cards/MissionCard.tsx
+++ src/components/ui/cards/MissionCard.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { useState } from "react";
 
 export interface MissionCardProps {
     className?: string;
@@ -11,10 +12,23 @@ export interface MissionCardProps {
 }
 
 const MissionCard = ({ className, title, description, image }: MissionCardProps) => {
+    const [isHovered, setIsHovered] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const isActive = isHovered || isOpen;
+
     return (
-        <div className={cn("card-mission", className)}>
+        <div
+            className={cn("card-mission", className, { "card-mission--open": isOpen })}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            onClick={handleClick}>
             <div className='card-mission__content-icon'>
-                <i className='ph ph-plus'></i>
+                <i className={isActive ? "ph ph-minus" : "ph ph-plus"}></i>
             </div>
             <div className='card-mission__image-container'>
                 <Image
